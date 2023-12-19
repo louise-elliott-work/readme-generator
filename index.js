496e18c858fd02bd4e00ffb459af239a9ae4df48
Louise Elliott <louise.elliott.work@gmail.com>
2023-12-19 15:59:04 +0000

    all data recorded correctly

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const inquirer = require("inquirer");
 // * array of questions for user
 const questions = [
     {
+        name: 'username',
+        type: 'input',
+        message: 'What is your GitHub username?'
+    },
+    {
+        name: 'email',
+        type: 'input',
+        message: 'What is your email address?'
+    },
+    {
         name: 'title',
         type: 'input',
         message: 'What is the title of your project?'
@@ -15,22 +25,10 @@ const questions = [
         message: 'How would you describe your project?'
     },
     {
-        name: 'licence',
-        type: 'list',
-        message: 'What licence do you want to use for this project?',
-        choices: ['none', 'Apache', 'GNU', 'MIT', 'Creative Commons']
-    },
-    {
-        name: 'dependencies',
+        name: 'installation',
         type: 'input',
         message: 'What command should be run to install dependencies?',
         default: 'npm i'
-    },
-    {
-        name: 'testing',
-        type: 'input',
-        message: 'What command should be run to carry out testing?',
-        default: 'npm test'
     },
     {
         name: 'usage',
@@ -38,9 +36,21 @@ const questions = [
         message: 'What should the user know in order to use this repo effectively?',
     },
     {
+        name: 'licence',
+        type: 'list',
+        message: 'What licence do you want to use for this project?',
+        choices: ['none', 'Apache', 'GNU', 'MIT', 'Creative Commons']
+    },
+    {
         name: 'contributions',
         type: 'input',
         message: 'What should the user do in order to contribute to the repo?',
+    },
+    {
+        name: 'testing',
+        type: 'input',
+        message: 'What command should be run to carry out testing?',
+        default: 'npm test'
     },
 ];
 
@@ -48,27 +58,30 @@ const questions = [
 inquirer
     .prompt(questions)
     .then((response) => {
-        const { title, description, licence, dependencies, testing, usage, contributions } = response;
+        const { title, description, installation, usage, licence, contributions, testing } = response;
         fs.writeFile('README.md',
         (`# ${title}
 ## Contents
-[description](#description)
-[licence](#licence)
-[dependencies](#dependencies)
-[testing](#testing)
-[usage](#usage)
-[contributions](#contributions)
+[Description](#description)<br>
+[Installation](#installation)<br>
+[Usage](#usage)<br>
+[Licence](#licence)<br>
+[Contributions](#contributions)<br>
+[Testing](#testing)<br>
 ## Description
 ${description}
-## Licence
-${licence}
 ## Dependencies
-${dependencies},
-## Testing
-${testing}
+${installation}
 ## Usage
 ${usage}
+## Licence
+This project is ${licence} licensed.
 ## Contributions
 ${contributions}
+## Testing
+${testing}
+## Questions
+To view the repo relating to this tool, please visit https://github.com/${username}.
+For any questions relating to this tool, please contact ${email}.
 `),
         (err) => err ? console.error(err) : console.log('README generated'))});
